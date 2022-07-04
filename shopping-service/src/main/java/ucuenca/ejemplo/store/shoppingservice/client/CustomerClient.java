41ab9a2275e728300913878a72c47eb2023ef42a
juanxcueva <jpandad@hotmail.com>
2022-07-04 18:25:48 -0500

    implementacion gateway

--- shopping-service/src/main/java/ucuenca/ejemplo/store/shoppingservice/client/CustomerClient.java
+++ shopping-service/src/main/java/ucuenca/ejemplo/store/shoppingservice/client/CustomerClient.java
@@ -1,16 +1,28 @@
 package ucuenca.ejemplo.store.shoppingservice.client;
 
+
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import ucuenca.ejemplo.store.shoppingservice.model.Customer;
-
 @FeignClient(name = "customer-service")
 public interface CustomerClient {
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/customers/{id}")
+    @CircuitBreaker(name = "customerCB",fallbackMethod = "getCustomerFallback")
     public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id);
+
+
+    default ResponseEntity<Customer> getCustomerFallback(RuntimeException e){
+        Customer customer = Customer.builder()
+        .firstName("none")
+        .lastName("none")
+        .email("none")
+        .photoUrl("none").build();
+    return ResponseEntity.ok(customer);
+    }
 }
--- shopping-service/src/main/java/ucuenca/ejemplo/store/shoppingservice/client/ProductClient.java
+++ shopping-service/src/main/java/ucuenca/ejemplo/store/shoppingservice/client/ProductClient.java
@@ -1,19 +1,36 @@
 package ucuenca.ejemplo.store.shoppingservice.client;
 
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import ucuenca.ejemplo.store.shoppingservice.model.Product;
 
-@FeignClient(name = "product-service")
+@FeignClient(name = "product-service",path = "/products")
 public interface ProductClient {
 
+    @CircuitBreaker(name = "productCB",fallbackMethod = "getProductFallback")
     @GetMapping(value = "/{id}")
     public ResponseEntity<Product> getProduct(@PathVariable("id") Long id);
 
+    @CircuitBreaker(name = "productUpdateCB",fallbackMethod = "getProductFallback")
     @GetMapping(value = "/{id}/stock")
-    public ResponseEntity<Product> updateStockProduct(@PathVariable  Long id ,@RequestParam(name = "quantity", required = true) Double quantity);
+    public ResponseEntity<Product> updateStock(@PathVariable("id")Long id, @RequestParam(name="stock",required = true)Double stock);
+
+
+    default ResponseEntity<Product> getProductFallback(RuntimeException e ){
+        // Product product=Product.builder()
+        //         .id(0L)
+        //         .name("none")
+        //         .description("none")
+        //         .price(0.0)
+        //         .stock(0.0)
+        //         .build();
+        return ResponseEntity.ok(null);
     }
+
+}
