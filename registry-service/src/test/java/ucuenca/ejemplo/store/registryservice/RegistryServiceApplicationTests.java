288435e8a7bdc326e2c739f7757ed1acad49d2fc
juanxcueva <jpandad@hotmail.com>
2022-07-04 12:05:48 -0500

    add registry service

---
+++ registry-service/src/test/java/ucuenca/ejemplo/store/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ucuenca.ejemplo.store.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
