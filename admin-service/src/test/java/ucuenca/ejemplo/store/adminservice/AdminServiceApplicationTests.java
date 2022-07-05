8cf643dd7688a202b255d8861fd883691f76c557
juanxcueva <jpandad@hotmail.com>
2022-07-04 19:21:59 -0500

    incorporacion de sleuth

---
+++ admin-service/src/test/java/ucuenca/ejemplo/store/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ucuenca.ejemplo.store.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
